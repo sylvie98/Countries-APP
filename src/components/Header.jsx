f5d9784a5db2dcc8e6edb477e1b01a4940b081a1
sylvie98 <sylviemasengesho5@gmail.com>
2024-04-02 09:41:42 +0200

    added responsive in navabar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,17 @@ const Header = () => {
   return (
     <section>
       <NavBar />
-      <div>
-     <img
+      
+      <div className="bg-[url('https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_1280.jpg')] bg-no-repeat bg-cover h-[400px]">
+
+     {/* <img
         src='https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_1280.jpg'
         alt="Background"
-        className="object-cover relative w-full h-[25rem]"/>
+        className="object-cover relative w-full h-[25rem]"/> */}
     </div>
     <div className='absolute top-0 left-0 w-full flex justify-center items-center flex-col  mt-24  p-5'>
     <h1 className='text-white text-1xl font-bold '>Country API App</h1>
-      <p className='text-white mt-6'>This is a simple app that allow you to search for countries and continents using the API</p>
+      <p className='text-white  text-2xl mt-6'>This is a simple app that allow you to search for countries and continents using the API</p>
       <input type='text'name='username' placeholder='search country by name' className='h-10 w-72 mt-6'/>
       </div>
     
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-      <section className="absolute top-0 left-0 w-full flex justify-around p-10">
-        <p className='text-blue-500 text-2xl font-bold'>Logoipsum</p>
-      <p className="text-white text-2xl font-bold ">Country</p>
-      
-    </section>
+    <section> 
+        <div className='absolute top-0 left-0 w-full flex justify-around p-10'>
+          <div className="flex justify-around">
+            <img src='src/image/log.svg'className="w-12 h-12 md:w-16 md:h-16" alt="logo" />
+            <p className="text-blue-500 text-lg md:text-3xl font-extrabold hidden sm:block">Logoipsum</p>
+          </div>
+          <div className="block sm:hidden">
+          <button className="text-white" onClick={toggleMenu}>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
+        <div className={`text-white text-sm md:text-base flex gap-6 md:gap-12 ${showMenu ? 'block' : 'hidden'} sm:block`}>
+          <li className='list-none'><a href=''>Countries</a></li>
+        </div>
+      </div>
+   </section>
   );
 };
 
